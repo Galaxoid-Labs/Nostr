3712f0548972ee30f12a98bb60c6a3e52be45624
Jacob Davis <ismyhc@gmail.com>
2024-06-15 08:53:08 -0400

    cleanup old code

--- Sources/Nostr/EventFilter.swift
+++ Sources/Nostr/EventFilter.swift
@@ -15,7 +15,7 @@ public struct EventFilter: Codable {
     public let since: Timestamp?
     public let until: Timestamp?
     public let limit: Int?
-
+    
     private let tags: [Tag]
     
     public init(
@@ -46,8 +46,8 @@ public struct EventFilter: Codable {
         try container.encodeIfPresent(limit, forKey: CustomCodingKeys(stringValue: "limit"))
         
         for tag in tags {
-            let key = tag.id.trimmingPrefix("#")
-            try container.encode(tag.otherInformation, forKey: CustomCodingKeys(stringValue: "#\(key)")) // We trim # just in case user passed it in, we add it ourselves
+            let key = tag.id.trimmingPrefix("#") // Since we will add #, we trim # prefix in case user added it themselves
+            try container.encode(tag.otherInformation, forKey: CustomCodingKeys(stringValue: "#\(key)"))
         }
     }
     
@@ -64,72 +64,3 @@ public struct EventFilter: Codable {
         }
     }
 }
-
-//public struct EventFilter: Codable {
-//    public let ids: [String]?
-//    public let authors: [String]?
-//    public let eventKinds: [EventKind]?
-//    public let eTags: [String]?
-//    public let pTags: [String]?
-//    public let hTags: [String]?
-//    public let dTags: [String]?
-//    public let aTags: [String]?
-//    public let since: Timestamp?
-//    public let until: Timestamp?
-//    public let limit: Int?
-//    
-//    private enum CodingKeys: String, CodingKey {
-//        case ids
-//        case authors
-//        case eventKinds = "kinds"
-//        case eTags = "#e"
-//        case hTags = "#h"
-//        case pTags = "#p"
-//        case dTags = "#d"
-//        case aTags = "#a"
-//        case since
-//        case until
-//        case limit
-//    }
-//    
-//    public init(
-//        ids: [String]? = nil,
-//        authors: [String]? = nil,
-//        eventKinds: [EventKind]? = nil,
-//        eTags: [String]? = nil,
-//        pTags: [String]? = nil,
-//        hTags: [String]? = nil,
-//        dTags: [String]? = nil,
-//        aTags: [String]? = nil,
-//        since: Timestamp? = nil,
-//        until: Timestamp? =  nil,
-//        limit: Int? = nil
-//    ) {
-//        self.ids = ids
-//        self.authors = authors
-//        self.eventKinds = eventKinds
-//        self.eTags = eTags
-//        self.pTags = pTags
-//        self.hTags = hTags
-//        self.dTags = dTags
-//        self.aTags = aTags
-//        self.since = since
-//        self.until = until
-//        self.limit = limit
-//    }
-//    
-//    public func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encodeIfPresent(ids, forKey: .ids)
-//        try container.encodeIfPresent(authors, forKey: .authors)
-//        try container.encodeIfPresent(eventKinds, forKey: .eventKinds)
-//        try container.encodeIfPresent(eTags, forKey: .eTags)
-//        try container.encodeIfPresent(pTags, forKey: .pTags)
-//        try container.encodeIfPresent(hTags, forKey: .hTags)
-//        try container.encodeIfPresent(dTags, forKey: .dTags)
-//        try container.encodeIfPresent(aTags, forKey: .aTags)
-//        try container.encodeIfPresent(since, forKey: .since)
-//        try container.encodeIfPresent(until, forKey: .until)
-//        try container.encodeIfPresent(limit, forKey: .limit)
-//    }
-//}
