f6354ae4ad2a96cbf2dc1bd71aea57c5d1fe58ac
Jacob Davis <ismyhc@gmail.com>
2024-08-25 17:04:03 -0400

    add inits for RelayInfo

--- Sources/Nostr/RelayInfo.swift
+++ Sources/Nostr/RelayInfo.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public struct RelayInfo: Codable {
-    
+
     public let description: String?
     public let name: String?
     public let publicKey: String?
@@ -34,9 +34,27 @@ public struct RelayInfo: Codable {
         case icon
         case contact
     }
+    
+    public init(description: String? = nil, name: String? = nil, publicKey: String? = nil, 
+                software: String? = nil, supportedNips: [Int], version: String? = nil,
+                limitation: Limitation? = nil, paymentsUrl: String? = nil,
+                fees: Fees? = nil, icon: String? = nil, contact: String? = nil) {
+        self.description = description
+        self.name = name
+        self.publicKey = publicKey
+        self.software = software
+        self.supportedNips = supportedNips
+        self.version = version
+        self.limitation = limitation
+        self.paymentsUrl = paymentsUrl
+        self.fees = fees
+        self.icon = icon
+        self.contact = contact
+    }
 }
 
 public struct Limitation: Codable {
+
     public let paymentRequired: Bool?
     public let maxMessageLength: Int?
     public let maxEventTags: Int?
@@ -50,14 +68,35 @@ public struct Limitation: Codable {
         case maxSubscriptions = "max_subscriptions"
         case authRequired = "auth_required"
     }
+    
+    public init(paymentRequired: Bool? = nil, maxMessageLength: Int? = nil, maxEventTags: Int? = nil,
+                maxSubscriptions: Int? = nil, authRequired: Bool? = nil) {
+        self.paymentRequired = paymentRequired
+        self.maxMessageLength = maxMessageLength
+        self.maxEventTags = maxEventTags
+        self.maxSubscriptions = maxSubscriptions
+        self.authRequired = authRequired
+    }
 }
 
 public struct Fee: Codable {
+
     public let amount: Int?
     public let unit: String?
     public let period: Int?
+    
+    public init(amount: Int? = nil, unit: String? = nil, period: Int? = nil) {
+        self.amount = amount
+        self.unit = unit
+        self.period = period
+    }
 }
 
 public struct Fees: Codable {
+
     let subscription: [Fee]
+    
+    public init(subscription: [Fee]) {
+        self.subscription = subscription
+    }
 }
